51ad523bf65ac82bae13134e0a74150e8c871cd1
Yernar Smagulov <yernar.smagulov02@gmail.com>
2024-11-06 00:07:58 -0800

    fixing navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
                     </a>
 
                     <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label="Toggle Menu">
-                        <img src={isOpen ? ".assets/close.svg" : ".assets/menu.svg"} alt="toggle" className="w-6 h-6" />
+                        <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} alt="toggle" className="w-6 h-6" />
                     </button>
 
                     <nav className="sm:flex hidden">
