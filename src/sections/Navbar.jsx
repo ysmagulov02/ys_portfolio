0af3c781b85e936614071e2f27a9c262aae938a6
Yernar Smagulov <yernar.smagulov02@gmail.com>
2024-11-05 23:55:31 -0800

    adding a link to navbar

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
         <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
             <div className="max-w-7xl mx-auto">
                 <div className="flex justify-between items-center py-5 mx_auto c-space">
-                    <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+                    <a href="https://ysmagulov02.github.io/ys_portfolio/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
                         Yernar
                     </a>
 
